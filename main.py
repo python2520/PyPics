2bd302ae83616c827678857cf916134d7d5c1d30
thap03 <trthap@cpp.edu>
2024-04-25 15:08:18 -0700

    added feature to disable the combobox unless a picture is selected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import colorchooser
+from tkinter import filedialog, colorchooser
 from PIL import Image, ImageOps, ImageTk, ImageFilter
 from tkinter import ttk
 
@@ -17,40 +16,42 @@ file_path = ""
 def add_image():
     global file_path
     file_path = filedialog.askopenfilename(
-        initialdir= "C:\\Users\\toby\\SCHOOL\\rando") # file directory for specifying the pic (CHANGE)
-    image = Image.open(file_path) # open image from file path
-    # resize image
-    width, height = int(image.width / 2), int(image.height / 2)
-    image = image.resize((width, height), Image.BILINEAR) # <- BILINEAR is in pillow
-    canvas.config(width = image.width, height = image.height)
-    image = ImageTk.PhotoImage(image) # in pillow as well
-    # set image to canvas
-    canvas.image = image
-    canvas.create_image(0, 0, image = image, anchor = "nw")
+        initialdir="C:\\Users\\toby\\SCHOOL\\rando")  # FILE NAME FOR PICS
+    if file_path:
+        image = Image.open(file_path) # open image from file path
+        # resize image
+        width, height = int(image.width / 2), int(image.height / 2)
+        image = image.resize((width, height), Image.BILINEAR) # <- BILINEAR is in pillow
+        canvas.config(width=image.width, height=image.height)
+        image = ImageTk.PhotoImage(image) # in pillow as well
+        # set image to canvas
+        canvas.image = image
+        canvas.create_image(0, 0, image=image, anchor="nw")
+        # enable filter combobox
+        toggle_filter_combobox("normal")
 
-# change pen color
 def change_color():
     global pen_color
     # shows a color palette 
-    pen_color = colorchooser.askcolor(title = "Select Pen Color")[1]
+    pen_color = colorchooser.askcolor(title="Select Pen Color")[1]
 
-# change pen size
 def change_size(size):
     global pen_size
     pen_size = size
 
-# drawing 
 def draw(event):
     x1, y1 = (event.x - pen_size), (event.y - pen_size)
     x2, y2 = (event.x + pen_size), (event.y + pen_size)
-    canvas.create_oval(x1, y1, x2, y2, fill = pen_color, outline = '') # creates a bunch of ovals as line drawlings
+    canvas.create_oval(x1, y1, x2, y2, fill=pen_color, outline='') # creates a bunch of ovals as line drawlings
 
-# clear canvas
 def clear_canvas():
     canvas.delete("all")
+    # disable filter combobox
+    toggle_filter_combobox("disabled")
 
-# apply the filter to image 
 def apply_filter(filter):
+    if not file_path:
+        return
     image = Image.open(file_path)
     width, height = int(image.width / 2), int(image.height / 2)
     image = image.resize((width, height), Image.BILINEAR) # <- In pillow imports
@@ -69,25 +70,28 @@ def apply_filter(filter):
     canvas.image = image
     canvas.create_image(0, 0, image=image, anchor="nw")
 
+def toggle_filter_combobox(state):
+    filter_combobox.config(state=state)
+
 # Creates left frame
-left_frame = tk.Frame(root, width = 200, height = 600, bg = "white")
-left_frame.pack(side = "left", fill = "y")
+left_frame = tk.Frame(root, width=200, height=600, bg="white")
+left_frame.pack(side="left", fill="y")
 
 # canvas size
-canvas = tk.Canvas(root, width = 750, height = 600)
+canvas = tk.Canvas(root, width=750, height=600)
 canvas.pack()
 
 # Image Button
-image_button = tk.Button(left_frame, text = "Upload Image", command = add_image, bg = "white")
+image_button = tk.Button(left_frame, text="Upload Image", command=add_image, bg="white")
 image_button.pack(pady=15)
 
 # Color Button
 color_button = tk.Button(
-    left_frame, text="Change Pen Color", command = change_color, bg = "white")
+    left_frame, text="Change Pen Color", command=change_color, bg="white")
 color_button.pack(pady=5)
 
 # Pen Size 
-pen_size_frame = tk.Frame(left_frame, bg = "white")
+pen_size_frame = tk.Frame(left_frame, bg="white")
 pen_size_frame.pack(pady=5)
 
 # Pen size radio buttons
@@ -108,14 +112,14 @@ pen_large = tk.Radiobutton(
 pen_large.pack(side="left")
 
 # Clear Button
-clear_button = tk.Button(left_frame, text="Clear",
+clear_button = tk.Button(left_frame, text="Clear All",
                          command=clear_canvas, bg="#FF9797")
 clear_button.pack(pady=10)
 
 # Filter Combobox
 filter_label = tk.Label(left_frame, text="Select Filter", bg="white")
 filter_label.pack()
-filter_combobox = ttk.Combobox(left_frame, values=["Black and White", "Blur", "Emboss", "Sharpen", "Smooth"])
+filter_combobox = ttk.Combobox(left_frame, values=["Black and White", "Blur", "Emboss", "Sharpen", "Smooth"], state="disabled")
 filter_combobox.pack()
 
 filter_combobox.bind("<<ComboboxSelected>>",
